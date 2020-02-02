2bdac22c21b6e7b4d4251096996e3ba57049a642
Giedrius <giedrius@blokas.io>
2020-02-02 22:56:51 +0000

    WIP

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo $REPO_ROOT
---
+++ modep-mod-host/build-deb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd $DIR
+source ../env.sh
+
+
+
+popd
