fd51d086ea28983657968a07c5bb9b2128ca38ce
Giedrius <giedrius@blokas.io>
2020-02-03 00:26:27 +0000

    mod-host deb

--- env.sh
+++ env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-echo $REPO_ROOT
+DEB_ROOT=$DIR/debroot
--- modep-mod-host/build-deb.sh
+++ modep-mod-host/build-deb.sh
@@ -2,9 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-pushd $DIR
+cd $DIR
 source ../env.sh
 
-
-
+pushd src
+make -j4
+make DESTDIR=$DEB_ROOT PREFIX=/usr install
 popd
+
+fakeroot dpkg --build debroot modep-mod-host.deb
+
