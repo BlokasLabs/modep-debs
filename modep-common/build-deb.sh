47a102abe555c009d26dd9340429c6d092358838
Giedrius <giedrius@blokas.io>
2020-02-04 00:06:45 +0000

    modep-common package restructured to use debhelper

--- modep-common/build-deb.sh
+++ modep-common/build-deb.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
 source ../env.sh
 
-fakeroot dpkg --build debroot modep-common.deb
+dpkg-buildpackage --no-sign --build=binary
 
 popd
