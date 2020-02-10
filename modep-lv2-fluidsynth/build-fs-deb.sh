f9ee127215d1440fe5348ac2794ad82429812168
Giedrius <giedrius@blokas.io>
2020-02-10 11:07:35 +0000

    FluidPlug deb

---
+++ modep-lv2-fluidsynth/build-fs-deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+source ../env.sh
+
+PACKAGE=modep-fluidsynth
+VERSION=2.1.0
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+mkdir -p ${PKGFOLDER}
+cp -r src_fs/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+#echo y | debuild -b -us -uc
