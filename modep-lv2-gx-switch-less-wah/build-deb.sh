bfb6348f4cdb1f21e093df4b9b3e6a4f36cb15a2
Giedrius <giedrius@blokas.io>
2020-02-10 15:59:33 +0000

    modep-lv2-gx-switchless-wah

---
+++ modep-lv2-gx-switch-less-wah/build-deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+source ../env.sh
+
+PACKAGE=modep-lv2-gx-switchless-wah
+VERSION=0.1.0
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+echo y | debuild -b -us -uc
