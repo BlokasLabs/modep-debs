9a708e96448335cac7076afdd7eb77d1637c05e8
Giedrius <giedrius@blokas.io>
2020-02-10 11:55:07 +0000

    x42 midifilter deb

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DEB_ROOT=$DIR/debroot
+export REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export DEB_ROOT=$DIR/debroot
+
+export MODEP_LV2_DIR=/usr/modep/lv2
