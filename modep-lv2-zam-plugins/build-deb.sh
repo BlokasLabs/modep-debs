4dc52195ae73a6f44fae29df970dc339ee0f055f
Giedrius <giedrius@blokas.io>
2020-02-13 13:52:07 +0000

    modep-lv2-zam-plugins deb

---
+++ modep-lv2-zam-plugins/build-deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+source ../env.sh
+
+PACKAGE=modep-lv2-zam-plugins
+VERSION=3.12.0
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+echo y | debuild -b -us -uc
