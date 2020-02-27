af3a36d11cb77fd1fd70cadd2f4e978061e6d423
Giedrius <giedrius@blokas.io>
2020-02-27 15:59:33 +0000

    modep-btn-scripts deb package

---
+++ modep-btn-scripts/build-deb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+source ../env.sh
+
+PACKAGE=modep-btn-scripts
+VERSION=1.1.0
+
+pushd src
+./make_deb.sh
+mv modep-btn-scripts.deb ../${PACKAGE}_${VERSION}.deb
+popd
