5369555362c38325dd66cc3f05fdf4082060831d
Giedrius <giedrius@blokas.io>
2020-06-15 12:10:38 +0100

    libhylia package

---
+++ libhylia/build-deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+source ../env.sh
+
+PACKAGE=libhylia
+VERSION=1.0.1
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+echo y | debuild -b -us -uc
