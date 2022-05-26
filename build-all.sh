cd14bdbcbbbbabfbba0f4972f3d2e16acedd4c35
Giedrius <giedrius@blokas.io>
2022-05-26 15:16:41 +0100

    Added use of SKIP files to skip building all debs, added missing build dep to install script

--- build-all.sh
+++ build-all.sh
@@ -4,9 +4,14 @@ set -e
 
 for i in $(ls); do
 	if [ -d $i ]; then
-		echo Building $i...
 		pushd $i
-		./build-deb.sh
+		if [ ! -e SKIP ]; then
+			echo Building $i...
+			./build-deb.sh
+			touch SKIP
+		else
+			echo Skipping building $i due to SKIP file.
+		fi
 		popd
 	fi
 done
